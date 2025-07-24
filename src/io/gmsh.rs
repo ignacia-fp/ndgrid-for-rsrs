13055be8d5da4ddfd88ce139abc0d88f12b7d31e
Ignacia Piccardo <ignacia-fp@Ignacias-MacBook-Air.local>
2025-07-24 18:04:02 +0100

    debug pringts

--- src/io/gmsh.rs
+++ src/io/gmsh.rs
@@ -224,7 +224,9 @@ impl<T: FromStr, B: Builder<T = T, EntityDescriptor = ReferenceCellType>> GmshIm
         };
         println!("3");
         let mut line_n = 1;
+        println!("nodes:{:?}", nodes);
         for _ in 0..num_entity_blocks {
+            println!("current line: {:?}", nodes[line_n]);
             let [_entity_dim, _entity_tag, parametric, num_nodes_in_block] = nodes[line_n]
                 .split(" ")
                 .map(|i| i.parse::<usize>().unwrap())
